91d276f3254806e141d00e974aa0432e17c0d69c
Rafael Gomes <raafaelgomees10@gmail.com>
2024-03-27 14:07:55 -0300

    add class to cart some methods and tests

---
+++ src/cart.js
@@ -0,0 +1,30 @@
+import { find, remove } from 'lodash';
+
+export default class Cart {
+  items = [];
+
+  add(item) {
+    const itemToFind = { product: item.product };
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind);
+    }
+    this.items.push(item);
+  }
+
+  remove(product) {
+    remove(this.items, { product });
+  }
+
+  getTotal() {
+    return this.items.reduce((acc, item) => {
+      return acc + item.quantity * item.product.price;
+    }, 0);
+  }
+
+  checkout() {
+    return {
+      total: this.getTotal(),
+      items: this.items,
+    };
+  }
+}
---
+++ src/cart.spec.js
@@ -0,0 +1,78 @@
+import Cart from './cart';
+
+describe('Cart', () => {
+  let cart;
+  let product = {
+    title: 'Tenis Ous',
+    price: 35388,
+  };
+
+  let product2 = {
+    title: 'Shape Element',
+    price: 41872,
+  };
+
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
+  describe('getTotal', () => {
+    it('Should return 0 when getTotal is executed in a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
+
+    it('Should multiply quantity and price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2,
+      };
+      cart.add(item);
+      expect(cart.getTotal()).toEqual(70776);
+    });
+
+    it('Should ensure no more than on product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product,
+        quantity: 1,
+      });
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('Should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
+    });
+  });
+
+  describe('checkout', () => {
+    it('Should return an object with the total and the list of items', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 3,
+      });
+
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+  });
+});
